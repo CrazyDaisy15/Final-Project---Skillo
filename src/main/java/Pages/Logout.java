b6fb500f750fa804e75fa76e52fed2a2a1e8f94a
CrazyDaisy <musakova87@gmail.com>
2024-04-17 23:48:23 +0300

    Latest Commit

--- src/main/java/Pages/Logout.java
+++ src/main/java/Pages/Logout.java
@@ -12,29 +12,34 @@ import java.time.Duration;
 public class Logout {
     private final WebDriver driver;
 
-    @FindBy(xpath = "//*[@class='nav-link']//*[@class='fas fa-sign-out-alt fa-lg'l*)
-            private WebElement logoutButton;
+   @FindBy(xpath = "//*[@class='nav-link']//*[@class='fas fa-sign-out-alt fa-lg']")
+           private WebElement logoutButton;
+
+   private final WebDriverWait wait;
 
     public Logout(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(this.driver, this);
+        this.wait = new WebDriverWait(this.driver, Duration.ofSeconds(15));
     }
 
     public void clickLogoutButton() {
-        WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(15));
-        WebElement logoutButton = wait.until(ExpectedConditions.visibilityOf(driver.findElement
-                (By.xpath("//*[@class='nav-link']//*[@class='fas fa-sign-out-alt fa-lg']"))));
-        logoutButton.click();
+        wait.until(ExpectedConditions.elementToBeClickable(logoutButton)).click();
     }
+       // WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(15));
+        //WebElement logoutButton = wait.until(ExpectedConditions.visibilityOf(driver.findElement
+        //        (By.xpath("//*[@class='nav-link']//*[@class='fas fa-sign-out-alt fa-lg']"))));
+       // logoutButton.click();
+
 
     public String getMessageModalText() {
-        WebElement message = this.driver.findElement(By.xpath("//*[@id=\"toast-container\"]/div"));
-        WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(15));
-        wait.until(ExpectedConditions.visibilityOf(message));
+      //  WebElement message = this.driver.findElement(By.xpath("//*[@id=\"toast-container\"]/div"));
+      //  WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(15));
+        WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"toast-container\"]/div")));
         return message.getText();
 
-    public void clickBackButton() {
-        this.driver.navigate().back();
-    }
+     //   public void clickBackBrowserButton() {
+     //   driver.navigate().back();
+     //   }
 }
 }
